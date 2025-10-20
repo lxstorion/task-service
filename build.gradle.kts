1bb959fd759301058f33e2ab1d4edc3c76d8dc7e
lxstorion <vk.tensei@yandex.ru>
2025-10-20 04:58:46 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.tensei"
+version = "0.0.1-SNAPSHOT"
+description = "Backend RESTful task service based on Java 17 + Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
